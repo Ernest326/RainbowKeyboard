703a02ee027769622d6aaee52a54d70fe121b141
Ernest <ernest326648@gmail.com>
2021-03-06 19:55:00 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# RGB Keyboard
- Gorilla Tag RGB Keyboard
+<h1>Rainbow Monke Mod</h1>
+<p>This is a mod for gorilla tag which can be run using <a href="https://github.com/DeadlyKitten/MonkeModManager/releases/tag/v1.2.0">Monke Mod Manager</a></p>
+<h1>Instructions</h1>
+<ol>
+<li>Download <a href="https://github.com/DeadlyKitten/MonkeModManager/releases/tag/v1.2.0">Monke Mod Manager</a> and place it in your local files. Run <code>MonkeModManager.exe</code>, navigate and select <code>Gorilla Tag.exe</code> in the file dialog and click <code>Install</code></li>
+<li>Go to <code>BepInEx\Plguins</code> in your Gorilla Tag folder and place the <code>RainbowKeyboard.dll</code> in there</li>
+<li>Run Monke Tag like usual and become Monke</li>
+</ol>
+<h2>Config</h2>
+<ul>
+<li>This mod allows use to change settings such as <code>Cycle Speed</code>, <code>Random Colour</code> and <code>Brightness</code></li>
+<li>These settings can be found in the config file at <code>BepInEx\config\RainbowKeyboard.cfg</code></li>
+<<<<<<< HEAD
+</ul>
+This is litterally a copy and paste of my last mod lol
+=======
+</ul>
+>>>>>>> 23c20b49d02f64c77c6dfd2afac678f6a52c5e0e
---
+++ RGB_Keyboard/RGBKeyboard.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Photon.Pun;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+namespace RainbowMonke
+{
+	[BepInPlugin("org.ernest326.gorillatag.rainbowkeyboard", "Rainbow Keyboard", "1.0.0")]
+	public class RGBKeyboard : BaseUnityPlugin
+	{
+		public static ConfigEntry<bool> enabled;
+		public static ConfigEntry<bool> randomColor;
+		public static ConfigEntry<float> cycleSpeed;
+		public static ConfigEntry<float> glowAmount;
+
+		private Color color = new Color(0, 0, 0);
+		private float hue = 0f;
+		private float timer = 0f;
+
+		private void Awake()
+		{
+
+			Debug.Log("Starting Rainbow Monkey");
+			ConfigFile config = new ConfigFile(Path.Combine(Paths.ConfigPath, "RainbowKeyboard.cfg"), true);
+
+			enabled = config.Bind<bool>("Config", "Enabled", true, "Whether the plugin is enabled or not");
+			randomColor = config.Bind<bool>("Config", "RandomColor", false, "Whether to cycle through colours of rainbow or choose random colors");
+			cycleSpeed = config.Bind<float>("Config", "CycleSpeed", 0.0025f, "The speed the color cycles at each frame (1=Full colour cycle). If random colour is enabled, this is the time in seconds before switching color");
+			glowAmount = config.Bind<float>("Config", "GlowAmount", 1.0f, "The brightness of your monkey. The higher the value, the more emissive your monkey is");
+
+		}
+
+		public void Update()
+		{
+
+			if (randomColor.Value)
+			{
+				if (Time.time > timer)
+				{
+					color = Random.ColorHSV(0, 1, glowAmount.Value, glowAmount.Value, glowAmount.Value, glowAmount.Value);
+					timer = Time.time + cycleSpeed.Value;
+				}
+
+			} else
+			{
+				if (hue >= 1)
+				{
+					hue = 0;
+				}
+
+				hue += cycleSpeed.Value;
+				color = Color.HSVToRGB(hue, 1.0f * glowAmount.Value, 1.0f * glowAmount.Value);
+			}
+
+			foreach(GorillaKeyboardButton key in GameObject.FindObjectsOfType<GorillaKeyboardButton>())
+			{
+				key.GetComponent<Renderer>().material.color = color;
+			}
+
+		}
+
+	}
+}
